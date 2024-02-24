28f15fd1ac2714184e494282b461e6346c2bd254
Emrys <158855872+IEmrys1@users.noreply.github.com>
2024-02-24 12:01:01 +0300

    Add files via upload

---
+++ Grade marks.cpp
@@ -0,0 +1,33 @@
+//program to find grade
+#include <stdio.h>
+int main(){
+int maths, eng, kisw, average;
+printf("Enter the marks for math, eng and kisw:\n ");
+scanf("%d %d %d", &maths, &eng, &kisw);
+
+average=(maths+eng+kisw)/3;
+if(average>=70 && average<=100)
+{
+printf("Grade A");
+}
+else if(average>=60 && average<=69)
+{
+printf("Grade B");
+}else if(average>=50 && average<=59)
+{
+printf("Grade C");
+
+}
+else if(average>=40 && average<=49)
+{
+printf("Grade D");
+
+}
+else if(average>=0&& average<=39)
+{
+printf("Grade F");
+}
+else ("Invalid Marks.");
+
+return 0;
+}
